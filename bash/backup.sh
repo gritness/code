3c30d1cfe51851fa518e64abaf8700c1ebf748a6
Rickey Chaman <rickey.chaman@yandex.com>
2021-09-07 21:44:18 -0300

    added backup

---
+++ bash/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BACKUPTIME=`date +%F-%T`
+
+SOURCEFOLDER1=/etc/seven
+
+SOURCEFOLDER2=/opt/seven
+
+SOURCEFOLDER3=/opt/wildfly/standalone/deployments
+
+DESTINATION=/backup/backup-$BACKUPTIME.tar.gz
+
+tar -cpzf $DESTINATION $SOURCEFOLDER1 $SOURCEFOLDER2 $SOURCEFOLDER3
