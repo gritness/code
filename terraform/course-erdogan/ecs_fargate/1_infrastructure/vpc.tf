1680840892f15f6f53e3bb805d7552b6357cdca4
gritness <gritness@yandex.com>
2021-11-26 01:18:19 -0300

    erdogan vpc

--- terraform/course-erdogan/ecs_fargate/1_infrastructure/vpc.tf
+++ terraform/course-erdogan/ecs_fargate/1_infrastructure/vpc.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 resource "aws_vpc" "prd-vpc" {
-    cidr_block = "${var.vpc_cidr}"
+    cidr_block = var.vpc_cidr
     enable_dns_hostnames = true
     tags = {
       "Name" = "prd-vcp"
@@ -15,8 +15,8 @@ resource "aws_vpc" "prd-vpc" {
 }
 
 resource "aws_subnet" "public-subnet-1" {
-    cidr_block = "${var.public_subnet_1_cidr}"
-    vpc_id = "${aws_vpc.prd-vpc.id}"
+    cidr_block = var.public_subnet_1_cidr
+    vpc_id = aws_vpc.prd-vpc.id
     availability_zone = "us-east-1a"
     tags = {
       "Name" = "public-subnet-1"
@@ -24,8 +24,8 @@ resource "aws_subnet" "public-subnet-1" {
 }
 
 resource "aws_subnet" "public-subnet-2" {
-    cidr_block = "${var.public_subnet_2_cidr}"
-    vpc_id = "${aws_vpc.prd-vpc.id}"
+    cidr_block = var.public_subnet_2_cidr
+    vpc_id = aws_vpc.prd-vpc.id
     availability_zone = "us-east-1b"
     tags = {
       "Name" = "public-subnet-2"
@@ -33,8 +33,8 @@ resource "aws_subnet" "public-subnet-2" {
 }
 
 resource "aws_subnet" "public-subnet-3" {
-    cidr_block = "${var.public_subnet_2_cidr}"
-    vpc_id = "${aws_vpc.prd-vpc.id}"
+    cidr_block = var.public_subnet_2_cidr
+    vpc_id = aws_vpc.prd-vpc.id
     availability_zone = "us-east-1c"
     tags = {
       "Name" = "public-subnet-3"
@@ -42,8 +42,8 @@ resource "aws_subnet" "public-subnet-3" {
 }
 
 resource "aws_subnet" "private-subnet-1" {
-    cidr_block = "${var.private_subnet_1_cidr}"
-    vpc_id = "${aws_vpc.prd-vpc.id}"
+    cidr_block = var.private_subnet_1_cidr
+    vpc_id = aws_vpc.prd-vpc.id
     availability_zone = "us-east-1a"
     tags = {
       "Name" = "private-subnet-1"
@@ -51,8 +51,8 @@ resource "aws_subnet" "private-subnet-1" {
 }
 
 resource "aws_subnet" "private-subnet-2" {
-    cidr_block = "${var.private_subnet_2_cidr}"
-    vpc_id = "${aws_vpc.prd-vpc.id}"
+    cidr_block = var.private_subnet_2_cidr
+    vpc_id = aws_vpc.prd-vpc.id
     availability_zone = "us-east-1b"
     tags = {
       "Name" = "private-subnet-2"
@@ -60,8 +60,8 @@ resource "aws_subnet" "private-subnet-2" {
 }
 
 resource "aws_subnet" "private-subnet-3" {
-    cidr_block = "${var.private_subnet_3_cidr}"
-    vpc_id = "${aws_vpc.prd-vpc.id}"
+    cidr_block = var.private_subnet_3_cidr
+    vpc_id = aws_vpc.prd-vpc.id
     availability_zone = "us-east-1c"
     tags = {
       "Name" = "private-subnet-3"
@@ -69,47 +69,47 @@ resource "aws_subnet" "private-subnet-3" {
 }
 
 resource "aws_route_table" "public-route-table" {
-    vpc_id = "${aws_vpc.prd-vpc.id}"
+    vpc_id = aws_vpc.prd-vpc.id
     tags = {
       "Name" = "public-route-table"
     }
 }
 
 resource "aws_route_table" "private-route-table" {
-    vpc_id = "${aws_vpc.prd-vpc.id}"
+    vpc_id = aws_vpc.prd-vpc.id
     tags = {
       "Name" = "private-route-table"
     }
 }
 
 resource "aws_route_table_association" "public-route-table-1-association" {
-    route_table_id = "${aws_route_table.public-route-table.id}"
-    subnet_id = "${aws_subnet.public-subnet-1.id}"
+    route_table_id = aws_route_table.public-route-table.id
+    subnet_id = aws_subnet.public-subnet-1.id
 }
 
 resource "aws_route_table_association" "public-route-table-2-association" {
-    route_table_id = "${aws_route_table.public-route-table.id}"
-    subnet_id = "${aws_subnet.public-subnet-2.id}"
+    route_table_id = aws_route_table.public-route-table.id
+    subnet_id = aws_subnet.public-subnet-2.id
 }
 
 resource "aws_route_table_association" "public-route-table-3-association" {
-    route_table_id = "${aws_route_table.public-route-table.id}"
-    subnet_id = "${aws_subnet.public-subnet-3.id}"
+    route_table_id = aws_route_table.public-route-table.id
+    subnet_id = aws_subnet.public-subnet-3.id
 }
 
 resource "aws_route_table_association" "private-route-table-1-association" {
-    route_table_id = "${aws_route_table.private-route-table.id}"
-    subnet_id = "${aws_subnet.private-subnet-1.id}"
+    route_table_id = aws_route_table.private-route-table.id
+    subnet_id = aws_subnet.private-subnet-1.id
 }
 
 resource "aws_route_table_association" "private-route-table-2-association" {
-    route_table_id = "${aws_route_table.private-route-table.id}"
-    subnet_id = "${aws_subnet.private-subnet-2.id}"
+    route_table_id = aws_route_table.private-route-table.id
+    subnet_id = aws_subnet.private-subnet-2.id
 }
 
 resource "aws_route_table_association" "private-route-table-3-association" {
-    route_table_id = "${aws_route_table.private-route-table.id}"
-    subnet_id = "${aws_subnet.private-subnet-3.id}"
+    route_table_id = aws_route_table.private-route-table.id
+    subnet_id = aws_subnet.private-subnet-3.id
 }
 
 resource "aws_eip" "elastic-ip-for-nat-gtw" {
@@ -118,4 +118,30 @@ resource "aws_eip" "elastic-ip-for-nat-gtw" {
     tags = {
       "Name" = "prd-eip"
     }
+}
+
+resource "aws_nat_gateway" "nat-gtw" {
+    allocation_id = aws_eip.elastic-ip-for-nat-gtw.id
+    subnet_id = aws_subnet.private-subnet-1.id
+    tags = {
+      "Name" = "prd-nat-gtw"
+    }
+
+    depends_on = ["aws_eip.elastic-ip-for-nat-gtw"]
+}
+
+resource "aws_route" "nat-gtw-route" {
+    route_table_id = aws_route_table.private-route-table.id
+    nat_gateway_id = aws_nat_gateway.nat-gtw.id
+    destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_internet_gateway" "prd-igw" {
+    vpc_id = aws_vpc.prd-vpc.id
+}
+
+resource "aws_route" "public-internet-gw-route" {
+    route_table_id = aws_route_table.public-route-table.id
+    gateway_id = aws_internet_gateway.prd-igw.id
+    destination_cidr_block = "0.0.0.0/0"
 }
